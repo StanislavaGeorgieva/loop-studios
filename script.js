b148bfdf8e62c9b0928df5ff317d36d54d485de4
Stanislava <hristot@smart-ss.org>
2025-08-11 13:39:35 +0300

    Hide hero text when mobile menu is open

--- script.js
+++ script.js
@@ -7,4 +7,6 @@ function navToggle() {
   btn.classList.toggle("open");
   menu.classList.toggle("flex");
   menu.classList.toggle("hidden");
+  // Toggle a class on body to hide other content when menu is open
+  document.body.classList.toggle("menu-open");
 }
